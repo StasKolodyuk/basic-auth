558744ddc34e93712b393995e2cd9089f5d32ba9
Stas Kolodyuk <staskolodyuk@gmail.com>
2017-10-14 23:40:00 +0300

    basic auth

---
+++ src/main/java/by/kolodyuk/basicauth/controller/InsecureController.java
@@ -0,0 +1,16 @@
+package by.kolodyuk.basicauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/insecure")
+public class InsecureController {
+
+    @GetMapping("/lol")
+    public String insecure() {
+        return "insecure lol";
+    }
+
+}
---
+++ src/main/java/by/kolodyuk/basicauth/controller/SecureController.java
@@ -0,0 +1,16 @@
+package by.kolodyuk.basicauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secure")
+public class SecureController {
+
+    @GetMapping("/lol")
+    public String insecure() {
+        return "secure lol";
+    }
+
+}
